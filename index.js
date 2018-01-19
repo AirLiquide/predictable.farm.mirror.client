f9a87971a88606fdcafee05cf846b37d1a6bd08e
briva <brivael.lepogam-sc@airliquide.com>
2018-01-19 17:15:12 +0100

    add open tunnel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//var socket = require('/usr/lib/node_modules/socket.io-client')('http://mirror.predictable.run:3000');
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('/usr/lib/node_modules/socket.io-client')('http://mirror.predictable.run:3000');
+//var socket = require('socket.io-client')('http://localhost:3000');
 var os = require("os");
 
 var exec = require('child_process').exec;
@@ -11,6 +11,7 @@ socket.on('connect', function() {
 socket.on('disconnect', function() {});
 
 socket.on('open_tunnel', function (port) {
+    exec('kill -9 $(pgrep -l ssh)');
     exec('ssh -R 0.0.0.0:'+port+'+:0.0.0.0:22 ubuntu@mirror.predictable.run -i id_rsa -y -N');
     socket.emit("tunnel_ok", os.hostname());
 });
